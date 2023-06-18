e1a5f88cc0b066e2da201c47c97a5f4cfb5cd92b
SAKulikova <kulikova.sonya1234@gmail.com>
2023-06-18 23:51:03 +0200

    Исправлена

--- Practice/zadacha2_2semestr/main.cpp
+++ Practice/zadacha2_2semestr/main.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
-int main() {
-    const int N = 10;
-    int mas_a [N] = {10,25,34,94,15,  36, 100, 3, 8, 13};
-    std::cout<<"То, что нужно скопировать: ";
-    for (int i=0;i<N;i++) //Копирование по одному символу, т.е. придется выполнить N копирований
-        std::cout<<mas_a[i];
-    std::cout<<std::endl;
 
-    int mas_b[N] = {0};
-    std::cout<<"До: ";
-    for (int i = 0;i < N;i++)
-        std::cout<<mas_b[i];
-    std::cout<<std::endl;
-
-    std::copy(std::begin(mas_a),std::end(mas_a),mas_b); //копирует машинные слова(т.е 32 или 64 бит)
-    std::cout<<"После: ";
-    for (int i=0;i<N;i++)
-        std::cout<<mas_b[i];
+void my_copy(int* source, int* destination, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        *(destination + i) = *(source + i);
+        std::cout<<destination[i]<<" ";
+    }
+}
+int main()
+{
+    const int size = 10;
+    int source [size] = {10,25,34,94,15,  36, 100, 3, 8, 13};
+    int destination[size]={};
+    my_copy(source,destination,size);
     return 0;
 }
